b45342bf2cb80f5e388852838d9ff61190f79f6d
ankingcodes <ankush.bhardwaj0@gmail.com>
2020-01-29 20:51:23 +0530

    init done

--- commands.py
+++ commands.py
@@ -5,8 +5,11 @@ from clint.textui import colored
 # init takes an input contest name and creates a directory
 # with that name and creates 5 files. Also it copies content 
 # from a template doc to each of the files.
+template = '#include<iostream>\nusing namespace std;\n\nint main(){ \n\n	return 0; \n}'
+
 def init(contestName):
 	print(colored.green('Make some files and folders for ' + contestName));
+	# create a directory with contest name
 	try:
 		path = os.getcwd()
 		os.mkdir(path + '/' + contestName)
@@ -14,6 +17,14 @@ def init(contestName):
 		print("Failed! This directory already exists.")
 	else:
 		print(colored.blue('Directory is made'))
+
+		# create files for contest (should be 6 cpp files)
+		fileNames = ['A.cpp','B.cpp','C.cpp','D.cpp','E.cpp','F.cpp']
+		for files in range(len(fileNames)):
+			f = open(path + '/' + contestName + '/' + fileNames[files],"w+")
+			f.write(template)
+			f.close()
+		print(colored.blue('Files have been created'))
 
 def listen(filename):
   print('listen to some file changes');
