99a71024b38f80ee76bf5b46d2836269573211b4
ankingcodes <ankush.bhardwaj0@gmail.com>
2020-01-30 19:45:04 +0530

    added dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
   version='0.0.1',
   author='Ankush Bhardwaj',
   packages=find_packages(),
+  install_requires=[
+    'watchdog',
+    'clint'
+  ],
   entry_points={
     'console_scripts': [
       'codemon = codemon.codemon:main'
