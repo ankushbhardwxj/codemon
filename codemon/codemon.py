285a7732ba42b7296bd5bdb68ce00adbdc69086f
ankingcodes <ankush.bhardwaj0@gmail.com>
2020-01-30 15:43:34 +0530

    listen gets filename

--- codemon/codemon.py
+++ codemon/codemon.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 import sys
 import os
+import time
+import logging
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
 from clint.textui import colored
+
+
 
 # init takes an input contest name and creates a directory
 # with that name and creates 5 files. Also it copies content 
@@ -9,17 +15,16 @@ from clint.textui import colored
 template = '#include<iostream>\nusing namespace std;\n\nint main(){ \n\n	return 0; \n}'
 
 def init(contestName):
-	print(colored.green('Make some files and folders for ' + contestName));
 	# create a directory with contest name
 	try:
+		print(colored.green('Make some files and folders for ' + colored.white(contestName)));
 		path = os.getcwd()
 		os.mkdir(path + '/' + contestName)
 	except OSError: 
 		print("Failed! This directory already exists.")
 	else:
 		print(colored.blue('Directory is made'))
-
-		# create files for contest (should be 6 cpp files)
+			# create files for contest (should be 6 cpp files)
 		fileNames = ['A.cpp','B.cpp','C.cpp','D.cpp','E.cpp','F.cpp']
 		for files in range(len(fileNames)):
 			f = open(path + '/' + contestName + '/' + fileNames[files],"w+")
@@ -27,8 +32,43 @@ def init(contestName):
 			f.close()
 		print(colored.blue('Files have been created'))
 
+	
+
+# when listen is used along with a filename, we listen for changes to that file 
+# whenever someone saves a file, we need to take inputs from an input.txt for the 
+# particular file, and then, 
+# g++ filename.cpp -o filename
+# ./filename < input 
+# time ./filename
+# show output on terminal 
+# keep listening continuously till ctrl + z is used on terminal
+# output should be : test case outputs, time for program , error in red.
+def isModified(event):
+	print(os.path.basename(event.src_path))
+
 def listen(filename):
-  print('listen to some file changes');
+	if os.path.exists(filename): 
+		print("Currently listening to it")
+		patterns = "*"
+		ignore_patterns = ""
+		ignore_directories = False
+		case_sensitive = True
+		path = os.getcwd()
+		event_handler = PatternMatchingEventHandler(patterns, ignore_patterns,ignore_directories,case_sensitive)
+		event_handler.on_modified = isModified 
+		observer = Observer()
+		observer.schedule(event_handler,path,recursive=True)
+		observer.start()
+		try:
+			while True:
+				time.sleep(1)
+		except KeyboardInterrupt:
+			observer.stop()
+		observer.join()
+	else:
+		print("File doesnt exist, check filename/path")
+	
+	
 
 def main():
 	command = sys.argv[0]
