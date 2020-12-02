c19a8b5d168efd06e594bc5682dd84a862ccc639
Aadhitya A <59508546+alphaX86@users.noreply.github.com>
2020-12-02 13:16:57 +0530

    Revert version to 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
   name='codemon',
-  version='0.0.2',
+  version='0.0.1',
   author='Ankush Bhardwaj',
   packages=find_packages(),
   install_requires=[
