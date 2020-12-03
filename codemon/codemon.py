776c65618f1f35c33c437e06253a6dafd45b5542
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2020-12-03 16:23:56 +0530

    Resolving conflicts from previous commit

--- codemon/codemon.py
+++ codemon/codemon.py
@@ -13,7 +13,6 @@ def main():
     showHelp()
 
   else:
-<<<<<<< HEAD
     countArg = 0;
     isCppFile = False
     isJavaFile = False
@@ -25,12 +24,6 @@ def main():
     # get all arguments and flags
     for arg in sys.argv:
       countArg+=1;
-=======
-    countArg = 0
-    for arg in sys.argv:
-      countArg+=1
-
->>>>>>> be25714d84b88b0bc96c1b795ecc5782d155730c
       if arg == "init":
         toInit = True
       elif arg == "-cpp":
@@ -50,43 +43,11 @@ def main():
     if toListen == True:
       listen()
 
-<<<<<<< HEAD
     elif toInit == True:
       if isJavaFile == True and isSingleFile == False:
         name = input("Enter Contest Name: ")
         files = get_filename_java()
         initjava(name, files)
-=======
-        elif sys.argv[countArg] == '-java':
-          
-          if sys.argv[countArg] == '-n':
-            file = sys.argv[countArg+1]
-            path = '.'
-            f = open(path + '/' + file + '.java',"w+")
-            template = template_java()
-            f.write(template)
-            f.close()
-            print(colored.yellow("Created "+file+'.java'))
-            break;
-          else:
-            contestName = sys.argv[countArg]
-            fileNames = get_filename_java()
-            initjava(contestName, fileNames)
-        if sys.argv[countArg] == '-n':
-          file = sys.argv[countArg+1]
-          path = '.'
-          f = open(path + '/' + file + '.cpp',"w+")
-          template = template_cpp()
-          f.write(template)
-          f.close()
-          print(colored.yellow("Created "+file+'.cpp'))
-          break
-
-        else:
-          contestName = sys.argv[countArg]
-          fileNames = get_filename()
-          init(contestName, fileNames)
->>>>>>> be25714d84b88b0bc96c1b795ecc5782d155730c
 
       elif isJavaFile == True and isSingleFile == True:
         # extension & path passed directly here
@@ -100,7 +61,6 @@ def main():
         files = get_filename()
         init(name, files)
 
-<<<<<<< HEAD
       elif isCppFile == True and isSingleFile == True:
         # extension & path passed directly here
         fileName = input("Enter filename: ")
@@ -117,21 +77,3 @@ def main():
         name = input("Enter Contest Name: ")
         practiceFiles = get_practice_files_java()
         initjava(name, practiceFiles)
-=======
-      elif arg == "practice":
-        if sys.argv[countArg] == '-cpp':
-          contestName = sys.argv[countArg]
-          practiceFiles = get_practice_files()
-          init(contestName, practiceFiles)
-        elif sys.argv[countArg] == '-java':
-          contestName = sys.argv[countArg]
-          practiceFiles = get_practice_files_java()
-          initjava(contestName, practiceFiles)
-        contestName = sys.argv[countArg]
-        practiceFiles = get_practice_files()
-        init(contestName, practiceFiles)
-
-      elif arg == "--help":
-        showHelp()
-        break
->>>>>>> be25714d84b88b0bc96c1b795ecc5782d155730c
