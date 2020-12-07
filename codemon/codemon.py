330d281d91a1cace02b0edebee0099f7e81bcfca
Enigmage <alizaidi0786.az@gmail.com>
2020-12-07 22:55:33 +0530

    efficiently parse command-line options

--- codemon/codemon.py
+++ codemon/codemon.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 import sys
 import os
@@ -8,49 +9,35 @@ from codemon.CodemonListen import listen
 from codemon.CodemonInit import init
 from codemon.CodemonMeta import template_cpp,get_filename, get_practice_files
 from codemon.CodemonFetch import fetch_tests, make_structure
+from codemon.CodemonParse import Commands
 
 def main():
-  if len(sys.argv) < 2:
+  arg = Commands()
+  arg.parse(sys.argv[1:])
+
+  if arg.help:
     showHelp()
 
+  elif arg.to_listen:
+    listen()
+
+  elif arg.to_practice:
+    contestName = sys.argv[countArg]
+    practiceFiles = get_practice_files()
+    init(contestName, practiceFiles)
+
+  elif arg.to_init:
+    if arg.init_flags["is_single"]:
+      fileName = arg.single_file_name
+      template = template_cpp()
+      init_single_file(f'{fileName}.cpp', template)
+      print(colored.yellow(f'Created {fileName}.cpp'))
+    else:
+      contestName = arg.contest_name
+      fileNames = get_filename(contestName)
+      init(contestName, fileNames)
   else:
-    countArg = 0
-    for arg in sys.argv:
-      countArg+=1
-
-      if arg == "init":
-        if sys.argv[countArg] == '-n':
-          fileName = sys.argv[countArg+1]
-          template = template_cpp()
-          init_single_file(f'{fileName}.cpp', template)
-          print(colored.yellow(f'Created {fileName}.cpp'))
-          break
-
-        else:
-          contestName = sys.argv[countArg]
-          fileNames = get_filename(contestName)
-          init(contestName, fileNames)
-
-      elif arg == "listen":
-        listen()
-
-      elif arg == "practice":
-        contestName = sys.argv[countArg]
-        practiceFiles = get_practice_files()
-        init(contestName, practiceFiles)
-
-      elif arg == "--help":
-        showHelp()
-        break
-      elif arg == "fetch":
-        try:
-          # if User provides contest number as arguement.
-          fetch_tests(sys.argv[countArg])
-        except IndexError:
-          # Extract all numbers from current directory name.
-          contest_number = ''.join(re.findall(r'\d+', os.getcwd().split('/')[-1]))
-          fetch_tests(contest_number)
-
+    showHelp()
 
 def init_single_file(filename, template='\n'):
   full_filename = os.path.join(os.getcwd(), filename)
