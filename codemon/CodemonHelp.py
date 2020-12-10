c15cadbbf213e7b6b550de2a31c61ef9622be48e
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2020-12-10 12:41:50 +0530

    Recreate help menu

--- codemon/CodemonHelp.py
+++ codemon/CodemonHelp.py
@@ -1,6 +1,9 @@
 from clint.textui import colored
 
 def showHelp():
+
+  print("\n")
+  #Logo font used is ANSI Shadow
   print(colored.green(""" ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ ██████╗ ███╗   ██╗
 ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔═══██╗████╗  ██║
 ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║██║   ██║██╔██╗ ██║
@@ -8,6 +11,8 @@ def showHelp():
 ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║
  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
                                                                """))
+  print("\n")
+  
   print("A CLI tool to ace competitive programming contests")
   print(colored.cyan("\nCOMMANDS: \n"))
   print("codemon - - - - - - - - - - - - - - -  displays this message")
@@ -15,4 +20,5 @@ def showHelp():
   print("codemon init <contestName> - - - - - - initialises a contest")
   print("codemon init -n <file> - - - - - - - - creates file with given name")
   print("codemon listen - - - - - - - - - - - - compiles and gives output")
+  print("codemon reg - - - - - - - - - - - - -  Register user details")
   print("codemon practice <dirName> - - - - - - Starts a practice session")
