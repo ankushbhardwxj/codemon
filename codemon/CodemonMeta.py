a296599e68ce57f9f872f898a2a4125acad20500
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-12-14 03:19:29 +0000

    Added Python Template

--- codemon/CodemonMeta.py
+++ codemon/CodemonMeta.py
@@ -36,9 +36,47 @@ int main() {
   def default_py(self):
     now = datetime.now()
     template="""
-# author: @ankingcodes
+# author: @Siddhant-K-code
 # created: %s
-# Write your code here
+from os import path
+from io import BytesIO, IOBase
+import sys
+from heapq import heappush,heappop
+from functools import cmp_to_key as ctk
+from collections import deque,Counter,defaultdict as dd 
+from bisect import bisect,bisect_left,bisect_right,insort,insort_left,insort_right
+from itertools import permutations
+from datetime import datetime
+from math import ceil,sqrt,log,gcd
+def ii():return int(input())
+def si():return input().rstrip()
+def mi():return map(int,input().split())
+def li():return list(mi())
+# mod=1000000007
+mod=998244353
+inf = float("inf")
+file = 1
+ 
+# write fastio for getting fastio template.
+    
+ 
+def solve():
+ 
+ 
+    # Your Solution
+
+
+        
+if __name__ =="__main__":
+ 
+    if(file):
+ 
+        if path.exists('input.txt'):
+            sys.stdin=open('input.txt', 'r')
+            sys.stdout=open('output.txt','w')
+        else:
+            input=sys.stdin.readline
+    solve()
    """ % (now)
     return template
 
