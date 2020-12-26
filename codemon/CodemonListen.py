dc52fc6f03373043839ad83d6e2cbaf9865d04fd
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2020-12-26 14:21:18 +0530

    fixed bugs on Listen and improved fetching templates

--- codemon/CodemonListen.py
+++ codemon/CodemonListen.py
@@ -42,9 +42,10 @@ def isModified(event):
 
 def compile_and_run(filename):
   # Store full file paths
-  full_filename = os.path.join(os.getcwd(), filename.split('.')[0], filename)
+  full_filename = os.path.join(os.getcwd(), filename)
   full_output_filename = os.path.join(os.getcwd(), 'prog')
   full_input_filename = os.path.join(os.getcwd(), 'test_case')
+  print(f"fullinputfilename: {full_input_filename}\n")
 
   # Check if required files exist
   if not Path(full_filename).is_file():
