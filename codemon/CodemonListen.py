f64c4534643c9f439901910bb769a8f6e60c7a5d
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2021-03-02 00:17:09 +0530

    fix running testcases on codemon

--- codemon/CodemonListen.py
+++ codemon/CodemonListen.py
@@ -11,22 +11,24 @@ from watchdog.observers import Observer
 
 class Runner:
   def __init__(self, fileName, dirName):
-    #self.src_file_path = os.path.join(os.getcwd(), filename.split('.')[0], filename)
     self.srcFilePath = os.path.join(dirName, fileName)
     self.testCaseFilePath = os.path.join(os.getcwd(), 'test_case')
     self.inputFilePath = os.path.join(dirName, f"{fileName.split('.')[0]}.in")
     self.outputFilePath = os.path.join(dirName, f"{fileName.split('.')[0]}.op")
 
-  def get_inputs_and_outputs(self):
-    user_in_list, sample_in_list, sample_out_list = [], [], []
-    with open(self.user_in_file_path, 'r+', encoding='utf-8') as cin:
-        #open(self.sample_in_file_path, 'r+', encoding='utf-8') as sin, \
-        #open(self.sample_out_file_path, 'r+', encoding='utf-8') as sout:
-      user_in_list = self.clean_file_content(cin.read())
-      #sample_in_list = self.clean_file_content(sin.read())
-      #sample_out_list = self.clean_file_content(sout.read())
-    #return user_in_list, sample_in_list, sample_out_list
-    return user_in_list
+  def getProgramMetadata(self, typeOfDir):
+    testCaseFile, inputFile, outputFile = [], [], [] 
+    if typeOfDir == 'contest':
+      with open(self.testCaseFilePath, 'r+', encoding='utf-8') as testCaseInput,\
+          open(self.inputFilePath, 'r+', encoding='utf-8') as sampleInput,\
+          open(self.outputFilePath, 'r+', encoding='utf-8') as sampleOutput:
+        testCaseFile = self.clean_file_content(testCaseInput.read())
+        inputFile = self.clean_file_content(sampleInput.read())
+        outputFile = self.clean_file_content(sampleOutput.read())
+    if typeOfDir == 'single':
+      with open(self.testCaseFilePath, 'r+', encoding='utf-8') as testCaseInput:
+        testCaseFile = self.clean_file_content(testCaseInput.read())
+    return testCaseFile, inputFile, outputFile
 
   def clean_file_content(self, content):
     res, temp = [], []
@@ -44,59 +46,56 @@ class Runner:
         temp = []
     return res
 
-  def run_cpp(self):
-    print(colored.cyan(f"Compiling {os.path.basename(self.src_file_path)}..."))
-    cpp_executable_path = os.path.join(os.getcwd(), 'prog')
-    # compiles the C++ program
-    compilation_child_process = subprocess.Popen(['g++', self.src_file_path, 
-      '-o', cpp_executable_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    compilation_child_process.wait()
-    compilation_child_process.terminate()
-    if compilation_child_process.returncode != 0:
-      return_code = compilation_child_process.returncode
+  def compileTarget(self, executable):
+    compiledProcess = subprocess.Popen(['g++', self.srcFilePath, '-o', executable], 
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    compiledProcess.wait()
+    compiledProcess.terminate()
+    return compiledProcess.returncode
+
+  def runExec(self, executable, inp):
+    execProcess = subprocess.Popen([executable], 
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    execProcess.stdin.write(inp.encode(encoding='utf-8'))
+    return execProcess
+
+  def run_cpp(self, typeOfDir):
+    print(colored.cyan(f"Compiling {os.path.basename(self.srcFilePath)}..."))
+    executable = os.path.join(os.getcwd(), 'prog')
+    returnCode = self.compileTarget(executable)
+    if returnCode != 0:
       if return_code == 127:
-        print("'g++' isn't installed.", file=sys.stderr)
+        print(colored.red("'g++' isn't installed.", file=sys.stderr))
       return
-    print('Running...')
-    # Runs executable, with testcases
-    #user_in_list, sample_in_list, sample_out_list = self.get_inputs_and_outputs()
-    user_in_list = self.get_inputs_and_outputs()
-    if user_in_list:
-      print(colored.yellow("Taking inputs from test_case file"))
-      for inp in user_in_list:
-        execution_child_process = subprocess.Popen([cpp_executable_path],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        execution_child_process.stdin.write(inp.encode(encoding='utf-8'))
-        current_output = execution_child_process.communicate()
-        if(len(current_output) > 0):
-          self.display_output(current_output[0].decode())
-        execution_child_process.terminate()
+    testCases, sampleInputs, sampleOutputs = self.getProgramMetadata(typeOfDir)
+    if testCases:
+      print(colored.yellow("Running inputs from test_case file..."))
+      for inp in testCases:
+        execTarget = self.runExec(executable, inp)
+        execOutput = execTarget.communicate()
+        if(len(execOutput) > 0): self.showOutput(execOutput[0].decode())
+        execTarget.terminate()
+    elif sampleInputs and sampleOutputs:
+      print(colored.yellow("Running sample testcases..."))
+      for inp, outp in zip(sampleInputs, sampleOutputs):
+        execTarget = self.runExec(executable, inp)
+        execOutput = execTarget.communicate()
+        if(len(execOutput) > 0): self.showOutput(execOutput[0].decode(), outp)
+        execTarget.terminate()
     else:
       print(colored.yellow("No input found."))
       print("Output: ")
-      subprocess.run(f'{cpp_executable_path}')
-    """
-    elif sample_in_list and sample_out_list:
-      print(colored.yellow("No custom input found."))
-      print(colored.yellow("Running sample testcases."))
-      for inp, outp in zip(sample_in_list, sample_out_list):
-        execution_child_process = subprocess.Popen([cpp_executable_path],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        execution_child_process.stdin.write(inp.encode(encoding='utf-8'))
-        current_output = execution_child_process.communicate()
-        if(len(current_output) > 0):
-          self.display_output(current_output[0].decode(), outp)
-        execution_child_process.terminate()
-    """
+      subprocess.run(f'{executable}')
 
   def run_py(self):
     python_interpreter = "python" if os.name == "nt" else "python3"
-    user_in_list, sample_in_list, sample_out_list = self.get_inputs_and_outputs()
+    testCases, sampleInputs, sampleOutputs = self.getProgramMetadata(typeOfDir)
     print("Running...")
-    if user_in_list:
+    if testCases:
       print(colored.yellow("Taking inputs from test_case file"))
-      for inp in user_in_list:
-        execution_child_process = subprocess.Popen([python_interpreter, self.src_file_path], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+      for inp in testCases:
+        execution_child_process = subprocess.Popen([python_interpreter, self.srcFilePath],
+            stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
         current_output, err = execution_child_process.communicate(input=inp.encode(encoding='utf-8'))
         if(execution_child_process.returncode != 0):
           print(err.decode())
@@ -104,35 +103,34 @@ class Runner:
         if(current_output):
           self.display_output(current_output.decode())
         execution_child_process.terminate()
-    elif sample_in_list and sample_out_list:
+    elif sampleInputs and sampleOutputs:
       print(colored.yellow("No custom input found."))
       print(colored.yellow("Running sample testcases."))
-      for inp, outp in zip(sample_in_list, sample_out_list):
-        execution_child_process = subprocess.Popen([python_interpreter, self.src_file_path], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+      for inp, outp in zip(sampleInputs, sampleOutputs):
+        execution_child_process = subprocess.Popen([python_interpreter, self.srcFilePath], 
+            stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
         current_output, err = execution_child_process.communicate(input=inp.encode(encoding='utf-8'))
         if(execution_child_process.returncode != 0):
           print(err.decode())
           return
         if(current_output):
-          self.display_output(current_output.decode(), outp)
+          self.showOutput(current_output.decode(), outp)
         execution_child_process.terminate()
     else:
       print(colored.yellow("No input found."))
       print("Output: ")
-      subprocess.run([python_interpreter, self.src_file_path])
+      subprocess.run([python_interpreter, self.srcFilePath])
 
-  def display_output(self, output, *args):
+  def showOutput(self, output, *args):
     if args:
       output = ''.join(line.rstrip()+'\n' for line in output.splitlines())
       expected_output = ''.join(line.rstrip()+'\n' for line in args[0].splitlines())
       if(output == expected_output):
         print(colored.green(f"Sample testcase passed."))
-        print()
         print(colored.yellow("Output:"))
         print(output)
       else:
         print(colored.red(f"Sample testcase failed !"))
-        print()
         print(colored.yellow("Output:"))
         print(output)
         print(colored.yellow("Changes needed:"))
@@ -147,7 +145,6 @@ class Runner:
   def check_files(self):
     fName = os.path.basename(self.srcFilePath).split('.')[0]
     if len(fName) <= 1: 
-      # if its a contest dir
       if not Path(self.srcFilePath).is_file():
         print("Cannot find source file to be compiled at {os.path.relpath(self.srcFilePath)}")
         return False
@@ -158,14 +155,15 @@ class Runner:
       if not Path(self.outputFilePath).is_file():
         print(f"Cannot find output file at {os.path.relpath(self.outputFilePath)}")
         return False
+      return (True, 'contest')
     else:
-      # if its not a contest dir
       if not Path(self.srcFilePath).is_file():
         print("Cannot find source file to be compiled at {os.path.relpath(self.srcFilePath)}")
         return False
       if not Path(self.testCaseFilePath).is_file():
         print(f"Cannot find input file (test_case) at the current working directory.")
-    return True
+        return False
+      return (True, 'single')
 
   def color_diff(self, diff):
     for line in diff:
@@ -182,12 +180,10 @@ def isModified(event):
   modifiedFile = os.path.basename(event.src_path) 
   modifiedFileDirectory = os.path.dirname(event.src_path)
   execute = Runner(modifiedFile, modifiedFileDirectory)
-  if execute.check_files():
-    print(colored.yellow('\nChange made at '+ modifiedFile))
-    """
-    if(modifiedFile.split('.')[-1] == 'cpp'): execute.run_cpp()
-    elif(modifiedFile.split('.')[-1] == 'py'): execute.run_py()
-    """
+  fileCheckRes, typeOfDir = execute.check_files()
+  if fileCheckRes:
+    if(modifiedFile.split('.')[-1] == 'cpp'): execute.run_cpp(typeOfDir)
+    elif(modifiedFile.split('.')[-1] == 'py'): execute.run_py(typeOfDir)
     
 
 def listen():
@@ -198,7 +194,7 @@ def listen():
     print(colored.magenta("Currently listening for file changes"))
     patterns = ['*.cpp', '*.py']
     ignore_patterns = ['prog', '*.exe', '*.swp']
-    #ignore_directories = True # TODO : check otherwise
+    ignore_directories = True 
     case_sensitive = True
     event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, case_sensitive)
     event_handler.on_created = isModified 
